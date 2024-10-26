f3793633e0e0f0989f3e356512ad72f052c556b4
ReitererLukas <lukas-reiterer@gmx.at>
2024-10-26 19:02:10 +0200

    make logging options configurable

--- image/server-logger/main.py
+++ image/server-logger/main.py
@@ -2,6 +2,17 @@ import subprocess
 from datetime import datetime 
 import platform
 from manager import Manager
+import os
+
+
+def write_to_file(file, manager, line: str):
+    file.write(line)
+    
+def write_to_logs(file, manager,  line: str):
+    print(line.strip())
+
+def write_to_manager(file, manager: Manager,  line: str):
+    manager.feed_line(line)
 
 def main():
     exe_path = "./TABG.exe"
@@ -12,6 +23,14 @@ def main():
     process = subprocess.Popen([exe_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
     manager = Manager()
+    
+    write_options = []
+    if os.getenv("WRITE_TO_LOGS", 1) == 1:
+        write_options.append(write_to_logs)
+    if os.getenv("WRITE_TO_FILE", 0) == 1:
+        write_options.append(write_to_file)
+    if os.getenv("FEED_TO_MANAGER", 0) == 1:
+        write_options.append(write_to_manager)
     # Continuously read the output line by line
     try:
         with open(f"logs/log{datetime.now().strftime('%Y%m%d%H%M%S')}.txt", "w") as file:
@@ -19,9 +38,8 @@ def main():
             while True:
                 output = process.stdout.readline()
                 if output:
-                    # print(output.strip())
-                    # manager.feed_line(output)
-                    file.write(output)
+                    for o in write_options:
+                        o(file, manager, output)
         
         # Read the remaining output
         stderr = process.communicate()[1]
